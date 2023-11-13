01d413de589458622ac2a727e43eee13897492be
Justin Bobrick <j.bobrick@outlook.com>
2023-11-13 15:17:44 -0800

    added code to generate function that asks for length

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-  // Password Generate Button
+// Password Generate Button
 var generateBtn = document.querySelector("#generate");
 
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numbers = '0123456789';
-var symbols = '!@#$%^&*_=+-/';
+// Variables for password criteria
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_=+-/";
 
 // Function to write password
 function writePassword() {
@@ -12,33 +13,46 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Function to generate password
 function generatePassword() {
   var characters = "";
   var password = "";
-// Prompts for password criteria
-  var lowercase = window.confirm("Would you like to include lowercase letters?");
-  var uppercase = window.confirm("Would you like to include uppercase letters?");
-  var numbers = window.confirm("Would you like to include numbers?");
-  var symbols = window.confirm("Would you like to include symbols?");
-  var length = window.prompt("How many characters would you like your password to be?");
-  if (lowercase) {
+  // Prompts for password criteria
+  var confirmLowercase = window.confirm(
+    "Would you like to include lowercase letters?"
+  );
+  var confirmUppercase = window.confirm(
+    "Would you like to include uppercase letters?"
+  );
+  var confirmNumbers = window.confirm("Would you like to include numbers?");
+  var confirmSymbols = window.confirm("Would you like to include symbols?");
+  var length = window.prompt(
+    "How many characters would you like your password to be?"
+  );
+  if (confirmLowercase) {
     characters += lowercase;
   }
-  if (uppercase) {
+  if (confirmUppercase) {
     characters += uppercase;
   }
-  if (numbers) {
+  if (confirmNumbers) {
     characters += numbers;
   }
-  if (symbols) {
+  if (confirmSymbols) {
     characters += symbols;
   }
+  // If statement to ensure password length is between 8 and 128 characters
+  if (length < 8 || length > 128) {
+    window.alert("Password length must be between 8 and 128 characters long.");
+    return generatePassword();
+  }
+  // For loop to generate password
   for (var i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
   }
   return password;
 }
